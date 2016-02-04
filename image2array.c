eb9c6ee2bab31603e34abfbee9fef984e875232d
jendralhxr <jendral.hxr@gmail.com>
2016-02-04 10:53:46 +0900

    image and line beginning marker (0xc, 0xd)

--- image2array.c
+++ image2array.c
@@ -9,6 +9,7 @@
 // gcc image2array.c `pkg-config --libs --cflags opencv`
 
 char string[40]; 
+unsigned char temp;
 int fd;
 int main(int argc, char **argv){
     fd = open("fooimage",O_WRONLY | O_CREAT, S_IRWXU);
@@ -17,30 +18,31 @@ int main(int argc, char **argv){
     if(!matA) return -1;
     else printf("%s is %dx%d\n",argv[1],matA->cols,matA->rows);
 
-	int loopY, loopX;
-        for( loopY = 50;loopY <100;++loopY)
-        {
-                for( loopX = 100;loopX < 150;++loopX)
-                {
-                        matA->data.ptr[matA->step*loopY+loopX] = 255;
-                }
-        }
-
-	int i, j, k;
+	int i, j, k=0;
     for (j=0; j<matA->rows; j++){
-		sprintf(string,"data[%d:%d] = 8'h0d;\n", k*8+7, k*8);
-		write(fd, string, strlen(string));
-		k++;
-		sprintf(string,"data[%d:%d] = 8'h0dh;\n", k*8+7, k*8);
-		write(fd, string, strlen(string));
-		k++;
+		if ((j==0) & (i==0)){
+			sprintf(string,"data[%d:%d] = 8'h0c;\n", k*8+7, k*8);
+			write(fd, string, strlen(string));
+			k++;
+			sprintf(string,"data[%d:%d] = 8'h0c;\n", k*8+7, k*8);
+			write(fd, string, strlen(string));
+			k++;
+			}
+		else{
+			sprintf(string,"data[%d:%d] = 8'h0d;\n", k*8+7, k*8);
+			write(fd, string, strlen(string));
+			k++;
+			sprintf(string,"data[%d:%d] = 8'h0dh;\n", k*8+7, k*8);
+			write(fd, string, strlen(string));
+			k++;
+			}
 		for (i=0; i<matA->cols; i++){	
-			sprintf(string,"data[%d:%d] = 8'h%x;\n", k*8+7, k*8, matA->data.ptr[j*matA->cols+i]);
+			temp= matA->data.ptr[j*matA->cols+i];
+			if ((temp!=0xc) && (temp!=0xd)) sprintf(string,"data[%d:%d] = 8'h%x;\n", k*8+7, k*8, temp);
+			else sprintf(string,"data[%d:%d] = 8'h0e;\n", k*8+7, k*8);
 			write(fd, string, strlen(string));
 			k++;
 			}	
 		}
     i++;
-    //sprintf(string,"data[%d:%d] = 8'0dh;\n", i*8+7, i*8);
-	//write(fd, string, strlen(string));
     }
